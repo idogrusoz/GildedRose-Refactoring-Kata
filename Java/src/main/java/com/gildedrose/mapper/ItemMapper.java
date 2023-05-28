09d0b09f465d74b7f8045920f8c246745b749675
Ibrahim Dogrusoz <idogrusoz@gmail.com>
2023-05-29 01:53:37 +0200

    Aged brie is mapped to maturing item

--- Java/src/main/java/com/gildedrose/mapper/ItemMapper.java
+++ Java/src/main/java/com/gildedrose/mapper/ItemMapper.java
@@ -2,9 +2,20 @@ package com.gildedrose.mapper;
 
 import com.gildedrose.Item;
 import com.gildedrose.model.BaseItem;
+import com.gildedrose.model.MaturingItem;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ItemMapper {
+
+    private static final List<String> MATURING_KEYWORDS = Arrays.asList("Aged Brie");
+
     public static BaseItem map(Item item) {
-        return new BaseItem(item);
+        if (MATURING_KEYWORDS.stream().anyMatch(keyword -> item.name.contains(keyword))) {
+            return new MaturingItem(item);
+        } else {
+            return new BaseItem(item);
+        }
     }
 }
--- Java/src/test/java/com/gildedrose/mapper/ItemMapperTest.java
+++ Java/src/test/java/com/gildedrose/mapper/ItemMapperTest.java
@@ -2,6 +2,7 @@ package com.gildedrose.mapper;
 
 import com.gildedrose.Item;
 import com.gildedrose.model.BaseItem;
+import com.gildedrose.model.MaturingItem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,5 +13,11 @@ class ItemMapperTest {
     void givenOrdinaryItem_whenMapped_thenItReturnsBaseItem() {
         BaseItem testItem = ItemMapper.map(new Item("test item", 10, 10));
         assertEquals(BaseItem.class, testItem.getClass());
+    }
+
+    @Test
+    void givenAgedBrie_whenMapped_thenItReturnsMaturingItem() {
+        BaseItem testItem = ItemMapper.map(new Item("Aged Brie test item", 10, 10));
+        assertEquals(MaturingItem.class, testItem.getClass());
     }
 }
